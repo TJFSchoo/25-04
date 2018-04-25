61a4b6abf78084ae2fac0517c93482a754473bf6
Tom Schoonbeek <tjfschoo@avans.nl>
2018-04-25 13:57:28 +0200

    Text altered

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
  
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hallo ik ben developer ja')
 })
  
 app.listen(3000)
